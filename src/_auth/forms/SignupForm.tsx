5d513d8493e5893a56af97ccba5403c6c7d40bbe
martinChakarov1337 <martin.chakarov121@gmail.com>
2024-04-20 19:47:50 +0300

    SigninForm added

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -133,7 +133,7 @@ const SignupForm = () => {
 
         <p className="text-small-regular text-light-2 text-center mt-2">
           Already have an account?
-          <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1"> Log in</Link>
         </p>
       </form>
       </div>
