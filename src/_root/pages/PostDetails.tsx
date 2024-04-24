be00a554eff46819076833e0d849aa4b9e2384d8
martinChakarov1337 <martin.chakarov121@gmail.com>
2024-04-24 12:10:44 +0300

    commit postdetails

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -7,14 +7,11 @@ import { Loader } from "lucide-react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
-
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id);
   const { user } = useUserContext();
-
   const { mutate: deletePost } = useDeletePost();
-
-  const  navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleDeletePost = () => {
     deletePost({ postId: id, imageId: post?.imageId || '' });
@@ -26,7 +23,6 @@ const PostDetails = () => {
   }
 
   if (!post) {
-    // Handle the case where post is undefined
     return <div>No post found.</div>;
   }
 
@@ -34,42 +30,42 @@ const PostDetails = () => {
     <div className="post_details-container">
       <div className="post_details-card">
         <img
-          src={post.imageUrl || ''}
+          src={post?.imageUrl || ''}
           alt="post"
           className="post_details-img"
         />
         <div className="post_details-info">
           <div className="flex-between w-full">
-            <Link to={`/profile/${post.creator?.$id}`} className="flex items-center gap-3">
+            <Link to={`/profile/${post?.creator?.$id}`} className="flex items-center gap-3">
               <img 
-                src={post.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}
+                src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}
                 alt="creator"
                 className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
               />
               <div className="flex flex-col">
                 <p className="base-medium lg:body-bold text-light-1">
-                  {post.creator?.name}
+                  {post?.creator?.name}
                 </p>
                 <div className="flex-center gap-2 text-light-3">
                   <p className="subtle-semibold lg:small-regular">
-                    {timeAgo(post.$createdAt)}
+                    {timeAgo(post?.$createdAt)}
                   </p>
                   -
                   <p className="subtle-semibold lg:small-regular">
-                    {post.location}
+                    {post?.location}
                   </p>
                 </div>
               </div>
             </Link>
           
             <div className="flex-center">
-              <Link to={`/update-post/${post.$id}`} className={`${user.id !== post.creator?.$id && 'hidden'}`}>
+              <Link to={`/update-post/${post?.$id}`} className={`${user.id !== post?.creator?.$id && 'hidden'}`}>
                 <img src="/assets/icons/edit.svg" width={24} height={24} alt="edit" />
               </Link>
               <Button
                 onClick={handleDeletePost} 
                 variant="ghost"
-                className={`ghost_details-delete_btn ${user.id !== post.creator?.$id && 'hidden'}`}
+                className={`ghost_details-delete_btn ${user.id !== post?.creator?.$id && 'hidden'}`}
               >
                 <img 
                   src="/assets/icons/delete.svg" 
@@ -82,9 +78,9 @@ const PostDetails = () => {
           </div>
           <hr className="border w-full border-dark-4/80" />
           <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
-            <p>{post.caption}</p>
+            <p>{post?.caption}</p>
             <ul className="flex gap-1 mt-2">
-              {post.tags.map((tag: string) => (
+              {post?.tags.map((tag: string) => (
                 <li key={tag} className="text-light-3">#{tag}</li>
               ))}
             </ul>
